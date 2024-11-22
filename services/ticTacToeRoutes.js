5b88e899fb1285aacf3ff8ed4b057d9bc6b89ddb
DMisraa <danielmenahem90@gmail.com>
2024-11-22 19:47:38 +0200

    final server setup

--- services/ticTacToeRoutes.js
+++ services/ticTacToeRoutes.js
@@ -11,6 +11,7 @@ const game_Online = new Connect4_Online();
 
 
 export const ticTacToe_getData = async (req, res) => {
+    console.log("CORS Allowed Origin:", process.env.BASE_URL);
     let data;
     const document = await ticTacToe_Game.getAllData();
   
